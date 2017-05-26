9a3874f0024bfe39b5e40b41d83aea3c9705eced
C.D. Clark III <clifton.clark@gmail.com>
2017-05-26 10:44:48 -0500

    renamed adaptive quadrature class

---
+++ testing/CatchTests/RecursiveAdaptiveQuadratureTests.cpp
@@ -0,0 +1,49 @@
+#include "catch.hpp"
+#include "fakeit.hpp"
+
+#include "_1D/RecursiveAdaptiveQuadrature.hpp"
+#include "_1D/TrapezoidRule.hpp"
+
+namespace  RecursiveAdaptiveQuadratureTests {
+
+double linear_func(double x)
+{
+  return 2*x + 3;
+}
+
+double box_func(double x)
+{
+  if(x < 1)
+    return 0;
+  if(x <= 5)
+    return 1;
+  return 0;
+}
+
+TEST_CASE( "Testing adaptive quadrature with trapezoid rule on linear functions." ) {
+
+  _1D::RecursiveAdaptiveQuadrature<_1D::TrapezoidRule,double> integrate;
+  double I;
+
+  I = integrate( linear_func, 2, 5 );
+  REQUIRE( I == Approx( 5*5+5*3 - 2*2 - 2*3 ) );
+
+  I = integrate([](double x){return 2*x + 3;},2,5);
+  REQUIRE( I == Approx( 5*5+5*3 - 2*2 - 2*3 ) );
+
+  I = integrate([](double x){return linear_func(x);},2,5);
+  REQUIRE( I == Approx( 5*5+5*3 - 2*2 - 2*3 ) );
+
+}
+
+TEST_CASE( "Testing adaptive quadrature with trapezoid rule on box functions." ) {
+
+  _1D::RecursiveAdaptiveQuadrature<_1D::TrapezoidRule,double> integrate;
+  double I;
+
+  I = integrate( box_func, 0, 10 );
+  REQUIRE( I == Approx( 5-1 ).epsilon(1e-5) );
+
+}
+
+}
