5f36f18dd2187a51b77d30263a1c3b3c2d393a31
C.D. Clark III <clifton.clark@gmail.com>
2017-08-04 11:07:18 -0500

    added all-include header.

---
+++ src/Integrate.hpp
@@ -0,0 +1,21 @@
+#ifndef Integrate_hpp
+
+/** @file Integrate.hpp
+  * @brief 
+  * @author C.D. Clark III
+  * @date 08/04/17
+  */
+
+
+
+#endif // include protector
+#include "_1D/RecursiveAdaptiveQuadrature.hpp"
+#include "_1D/RiemannRule.hpp"
+#include "_1D/GaussianQuadratures/GaussLegendre.hpp"
+#include "_1D/TrapezoidRule.hpp"
+#include "_1D/SimpsonRule.hpp"
+
+#include "_2D/RiemannRule.hpp"
+#include "_2D/GaussianQuadratures/GaussLegendre.hpp"
+
+#define Integrate_hpp
