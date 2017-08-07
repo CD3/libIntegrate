b544b9ffc2320237b7eb64d80db62af88ee63c58
C.D. Clark III <clifton.clark@gmail.com>
2017-08-06 20:15:40 -0500

    added experimental reverse communication integration algorithm.

---
+++ src/Experimental/README.md
@@ -0,0 +1,1 @@
+This directory contains code that is experimental and should *not* be used in production code. The header files here are not included by the top level `Integrate.hpp` file.
--- testing/CatchTests/GaussianQuadratureTests.cpp
+++ testing/CatchTests/GaussianQuadratureTests.cpp
@@ -7,14 +7,10 @@
 namespace GausssLegendreTests
 {
 
+TEST_CASE( "Testing 1D GQ:8 rule on polynomials." ) {
 
-TEST_CASE( "Testing 1D GQ:64 rule on polynomials." ) {
-
-
-  _1D::GQ::GaussLegendreQuadrature<double,64> integrate;
-
-
-  for(int N = 0; N < 100; N++)
+  _1D::GQ::GaussLegendreQuadrature<double,8> integrate;
+  for(int N = 0; N < 8; N++)
   {
     auto f  = [&N](double x) { double sum = 0; for(int i = 0; i < N+1; i++){ sum = sum + pow(x,i); }; return sum; };
     auto fi = [&N](double x) { double sum = 0; for(int i = 0; i < N+1; i++){ sum = sum + pow(x,i+1)/(i+1); }; return sum; };
@@ -23,15 +19,74 @@ TEST_CASE( "Testing 1D GQ:64 rule on polynomials." ) {
     CHECK( integrate( f, 0., 100. ) == Approx( fi(100) - fi(0) ) );
   }
 
+}
+
+
+TEST_CASE( "Testing 1D GQ:16 rule on polynomials." ) {
+
+  _1D::GQ::GaussLegendreQuadrature<double,16> integrate;
+  for(int N = 0; N < 16; N++)
+  {
+    auto f  = [&N](double x) { double sum = 0; for(int i = 0; i < N+1; i++){ sum = sum + pow(x,i); }; return sum; };
+    auto fi = [&N](double x) { double sum = 0; for(int i = 0; i < N+1; i++){ sum = sum + pow(x,i+1)/(i+1); }; return sum; };
+    CHECK( integrate( f, 0., 1. )  == Approx( fi(1)  - fi(0) ) );
+    CHECK( integrate( f, 1., 10. ) == Approx( fi(10) - fi(1) ) );
+    CHECK( integrate( f, 0., 100. ) == Approx( fi(100) - fi(0) ) );
+  }
 
 }
 
 
-TEST_CASE( "Testing 1D GQ:64 rule on trig functions." ) {
+TEST_CASE( "Testing 1D GQ:32 rule on polynomials." ) {
 
+  _1D::GQ::GaussLegendreQuadrature<double,32> integrate;
+  for(int N = 0; N < 32; N++)
+  {
+    auto f  = [&N](double x) { double sum = 0; for(int i = 0; i < N+1; i++){ sum = sum + pow(x,i); }; return sum; };
+    auto fi = [&N](double x) { double sum = 0; for(int i = 0; i < N+1; i++){ sum = sum + pow(x,i+1)/(i+1); }; return sum; };
+    CHECK( integrate( f, 0., 1. )  == Approx( fi(1)  - fi(0) ) );
+    CHECK( integrate( f, 1., 10. ) == Approx( fi(10) - fi(1) ) );
+    CHECK( integrate( f, 0., 100. ) == Approx( fi(100) - fi(0) ) );
+  }
+
+}
+
+
+TEST_CASE( "Testing 1D GQ:64 rule on polynomials." ) {
 
   _1D::GQ::GaussLegendreQuadrature<double,64> integrate;
+  for(int N = 0; N < 64; N++)
+  {
+    auto f  = [&N](double x) { double sum = 0; for(int i = 0; i < N+1; i++){ sum = sum + pow(x,i); }; return sum; };
+    auto fi = [&N](double x) { double sum = 0; for(int i = 0; i < N+1; i++){ sum = sum + pow(x,i+1)/(i+1); }; return sum; };
+    CHECK( integrate( f, 0., 1. )  == Approx( fi(1)  - fi(0) ) );
+    CHECK( integrate( f, 1., 10. ) == Approx( fi(10) - fi(1) ) );
+    CHECK( integrate( f, 0., 100. ) == Approx( fi(100) - fi(0) ) );
+  }
 
+}
+
+
+TEST_CASE( "Comparing 1D GQ:8 and GQ:64 rules on polynomials." ) {
+
+  _1D::GQ::GaussLegendreQuadrature<double,8> integrate8;
+  _1D::GQ::GaussLegendreQuadrature<double,64> integrate64;
+  // GQ order 8 should be *exact* up to 8th order polynomials.
+  // but it's actually pretty darn close for even higher orders...
+  for(int N = 0; N < 20; N++)
+  {
+    auto f  = [&N](double x) { double sum = 0; for(int i = 0; i < N+1; i++){ sum = sum + pow(x,i); }; return sum; };
+    CHECK( integrate8( f, 0., 1.   ) == Approx( integrate64( f, 0., 1.   ) ) );
+    CHECK( integrate8( f, 1., 10.  ) == Approx( integrate64( f, 1., 10.  ) ) );
+    CHECK( integrate8( f, 0., 100. ) == Approx( integrate64( f, 0., 100. ) ) );
+  }
+
+}
+
+
+
+TEST_CASE( "Testing 1D GQ:64 rule on trig functions." ) {
+  _1D::GQ::GaussLegendreQuadrature<double,64> integrate;
 
   CHECK( integrate( sin, 0., M_PI )  == Approx( 2 ) );
   CHECK( integrate( sin, M_PI, 2*M_PI )  == Approx( -2 ) );
@@ -40,7 +95,6 @@ TEST_CASE( "Testing 1D GQ:64 rule on trig functions." ) {
 }
 
 
-}
 
 TEST_CASE( "Testing 2D GQ:64 rule on simple linear function." ) {
 
@@ -53,15 +107,13 @@ TEST_CASE( "Testing 2D GQ:64 rule on simple linear function." ) {
   I = integrate( f, 2., 5., 3., 6. );
   CHECK( I == Approx( ( fi(5,6) - fi(5,3) ) - ( fi(2,6) - fi(2,3) ) ).epsilon(0.001) );
 
-
-
 }
 
 TEST_CASE( "Testing 2D GQ:64 rule on 2-var polynomial function." ) {
 
   _2D::GQ::GaussLegendreQuadrature<double,64> integrate;
 
-  for(int N = 0; N < 100; N = N + 4)
+  for(int N = 0; N < 64; N = N + 4)
   {
     auto f  = [&N](double x,double y)
     { double sum = 0;
@@ -89,6 +141,10 @@ TEST_CASE( "Testing 2D GQ:64 rule on trig functions." ) {
   CHECK( integrate( [](double x, double y){ return sin(x)*cos(y); },
                     0., M_PI,
                     0., M_PI ) == Approx( (-cos(M_PI)*(sin(M_PI) - sin(0)) + cos(0)*(sin(M_PI) - sin(0)) ) ) );
+
+
+}
+
 
 
 }
