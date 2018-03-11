75f09455948f05cd701f751cff830d7412f7ba8d
C.D. Clark III <clifton.clark@gmail.com>
2018-03-11 14:30:56 -0500

    added util script.

---
+++ util-scripts/tag-release.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+tag=$1
+shift
+
+if [[ -z $tag ]]
+then
+  echo "ERROR: version number required"
+  echo "example: $0 v1.0.1"
+  exit 1
+fi
+
+function exit_on_error()
+{
+  echo "There was an error. Commit will not be tagged."
+}
+
+trap exit_on_error ERR
+
+set -e
+root=$(git rev-parse --show-toplevel)
+
+echo "cd'ing to root directory ($root)"
+cd $root
+
+echo "looking for pre-tag-release.sh to run"
+$(find ./ -name 'pre-tag-release.sh')
+
+echo "tagging with ${tag}"
+git tag ${tag}
+
