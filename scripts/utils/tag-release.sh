661e83ea7736998306c3d6b049116537290f4999
C.D. Clark III <clifton.clark@gmail.com>
2019-11-13 19:12:33 -0600

    housekeeping: tag-release.sh now writes tag to version.txt
    We need to write the version tag to a text file so we can support
    building the library without git. For example, if a user downloads a zip
    file from github, it will not contain the git history.

--- scripts/utils/tag-release.sh
+++ scripts/utils/tag-release.sh
@@ -20,7 +20,6 @@ then
 fi
 
 
-exit
 function exit_on_error()
 {
   echo "There was an error. Commit will not be tagged."
@@ -37,6 +36,11 @@ cd $root
 echo "looking for pre-tag-release.sh to run"
 $(find ./ -name 'pre-tag-release.sh')
 
+echo "writing ${tag} to version.txt"
+echo "${tag}" > version.txt
+echo "commiting change"
+git add version.txt
+git commit -m "housekeeping: bumped version in version.txt"
 echo "tagging with ${tag}"
 git tag ${tag}
 git tag | grep ${tag}
