6b805233092f470cd55256895d02b7d0f1984763
CD Clark III <clifton.clark@gmail.com>
2021-04-04 10:00:00 -0500

    devel: added support for unfirom discretized functions to simpson's rule.

--- src/libIntegrate/_1D/SimpsonRule.hpp
+++ src/libIntegrate/_1D/SimpsonRule.hpp
@@ -23,9 +23,102 @@ class SimpsonRule
            typename SFINAE = typename std::enable_if<(NN_ > 0)>::type>
   T operator()(F f, T a, T b) const;
 
-  // This version will integrate a set of discrete points
+  /**
+   * This version will integrate a discretized function with the x and y
+   * values held in separate containers.
+   *
+   * @param x a vector-like container with operator[](int) and .size()
+   * @param y a vector-like container with operator[](int)
+   *
+   * x and y must have the same size *and* contain 3 or more elements,
+   * othersize the call is undefined behavior.
+   */
   template<typename X, typename Y>
-  T operator()(X &x, Y &y) const;
+  auto operator()(const X &x, const Y &y) const -> decltype(x.size(),x[0],y[0],T())
+  {
+    T sum = 0;
+    decltype(x.size()) i;
+    for (i = 0; i < x.size() - 2; i += 2) {
+      // Integrate segment using three points
+      // \int_a^b f(x) dx = (b-a)/6 [ f(a) + 4*f(m) + f(b) ]
+      // a = x[i]
+      // b = x[i+2]
+      // m = (a+b)/2
+      //
+      // f(a) = y[i]
+      // f(b) = y[i+2]
+      // and we need to interpolate f(m)
+      // clang-format off
+      T m = (x[i] + x[i+2])/2;
+      T ym = y[i  ]*LagrangePolynomial(m, x[i+1], x[i+2], x[i  ])
+           + y[i+1]*LagrangePolynomial(m, x[i  ], x[i+2], x[i+1])
+           + y[i+2]*LagrangePolynomial(m, x[i  ], x[i+1], x[i+2]);
+
+      sum += (x[i+2]-x[i])/6 * (y[i] + 4*ym + y[i+2]);
+      // clang-format on
+    }
+
+    if( x.size() % 2 == 0) // note: this tests if the last *index* is odd. the size will be even.
+    {
+      if( x.size() > 2)
+      {
+      // there is on extra point at the end we need to handle
+      // we will use the last *three* points to fit the polynomial
+      // but then integrate between the last *two* points.
+      i = x.size()-3;
+      T m = (x[i+1] + x[i+2])/2;
+      T ym = y[i  ]*LagrangePolynomial(m, x[i+1], x[i+2], x[i  ])
+           + y[i+1]*LagrangePolynomial(m, x[i  ], x[i+2], x[i+1])
+           + y[i+2]*LagrangePolynomial(m, x[i  ], x[i+1], x[i+2]);
+
+      sum += (x[i+2]-x[i+1])/6 * (y[i+1] + 4*ym + y[i+2]);
+      }
+    }
+
+    return sum;
+  }
+
+  /**
+   * This version will integrate a uniformly discretized function with y
+   * values held in a container.
+   *
+   * @param y a vector-like container with operator[](int) and .size()
+   * @param dx a the spacing between consecutive function values.
+   *
+   * y must contain 3 or more elements, othersize the call is undefined behavior.
+   */
+  template<typename Y>
+  auto operator()(const Y &y, T dx = 1) const -> decltype(y.size(),dx*y[0],T())
+  {
+    T sum = 0;
+    decltype(y.size()) i;
+
+    for(i = 0; i < y.size()-2; i+=2)
+    {
+      sum += y[i] + 4*y[i+1] + y[i+2];
+    }
+    sum *= dx/3.;
+
+    // if the container size is even, there will be one extra interval we need to handle
+    if( y.size() % 2 == 0)
+    {
+      // use the last three points to interpolate
+      // the function to the midpoint of the last two
+      // points. Then apply Simpson's rule
+      // clang format off
+      i = y.size()-3;
+      T ym = y[i  ]*LagrangePolynomial(3.*dx/2 , dx , 2*dx , 0  )
+           + y[i+1]*LagrangePolynomial(3.*dx/2 , 0  , 2*dx , dx )
+           + y[i+2]*LagrangePolynomial(3.*dx/2 , 0  , dx   , 2*dx);
+
+      sum += dx/6 * (y[i+1] + 4*ym + y[i+2]);
+      // clang format on
+    }
+
+
+    return sum;
+  }
+
 
  protected:
   T LagrangePolynomial(T x, T A, T B, T C) const;
@@ -37,8 +130,8 @@ T SimpsonRule<T, NN>::operator()(F f, T a, T b, std::size_t N) const
 {
   T sum = 0;
   T dx  = static_cast<T>(b - a) / N;  // size of each interval
-  T x;
-  for (x = a; x < b; x += dx) {
+  T x = a;
+  for (std::size_t i = 0; i < N; i++, x += dx) {
     sum += f(x) + 4 * f(x + dx / 2) + f(x + dx);
   }
   // note that 2*h = dx
@@ -52,58 +145,14 @@ T SimpsonRule<T, NN>::operator()(F f, T a, T b) const
 {
   T sum = 0;
   T dx  = static_cast<T>(b - a) / NN;  // size of each interval
-  T x;
-  for (x = a; x < b; x += dx) {
+  T x = a;
+  for (std::size_t i = 0; i < NN; i++, x += dx) {
     sum += f(x) + 4 * f(x + dx / 2) + f(x + dx);
   }
   // note that 2*h = dx
   sum *= dx / 6;
   return sum;
 }
-
-template<typename T, std::size_t NN>
-template<typename X, typename Y>
-T SimpsonRule<T, NN>::operator()(X &x, Y &y) const
-{
-  T sum = 0;
-  decltype(x.size()) i;
-  for (i = 0; i < x.size() - 2; i += 2) {
-    // Integrate segment using three points
-    // \int_a^b f(x) dx = (b-a)/6 [ f(a) + 4*f(m) + f(b) ]
-    // a = x[i]
-    // b = x[i+2]
-    // m = (a+b)/2
-    //
-    // f(a) = y[i]
-    // f(b) = y[i+2]
-    // and we need to interpolate f(m)
-    // clang-format off
-    T m = (x[i] + x[i+2])/2;
-    T ym = y[i  ]*LagrangePolynomial(m, x[i+1], x[i+2], x[i  ])
-         + y[i+1]*LagrangePolynomial(m, x[i  ], x[i+2], x[i+1])
-         + y[i+2]*LagrangePolynomial(m, x[i  ], x[i+1], x[i+2]);
-
-    sum += (x[i+2]-x[i])/6 * (y[i] + 4*ym + y[i+2]);
-    // clang-format on
-  }
-
-  if( x.size() % 2 == 0) // note: this tests if the last *index* is odd. the size will be even.
-  {
-    // there is on extra point at the end we need to handle
-    // we will use the last *three* points to fit the polynomial
-    // but then integrate between the last *two* points.
-    i = x.size()-3;
-    T m = (x[i+1] + x[i+2])/2;
-    T ym = y[i  ]*LagrangePolynomial(m, x[i+1], x[i+2], x[i  ])
-         + y[i+1]*LagrangePolynomial(m, x[i  ], x[i+2], x[i+1])
-         + y[i+2]*LagrangePolynomial(m, x[i  ], x[i+1], x[i+2]);
-
-    sum += (x[i+2]-x[i+1])/6 * (y[i+1] + 4*ym + y[i+2]);
-  }
-
-  return sum;
-}
-
 template<typename T, std::size_t NN>
 T SimpsonRule<T, NN>::LagrangePolynomial(T x, T A, T B, T C) const
 {
--- testing/CatchTests/SimpsonRuleTests.cpp
+++ testing/CatchTests/SimpsonRuleTests.cpp
@@ -1,6 +1,8 @@
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
 #include "catch.hpp"
 
 #include<iostream>
+#include<numeric>
 #include <libIntegrate/_1D/SimpsonRule.hpp>
 
 namespace SimpsonRuleTests
@@ -69,7 +71,7 @@ TEST_CASE("Simpson rule on discretized functions.")
   SECTION("4 point data set")
   {
     // Simpson's rule will use three points at a time. If the data sent in is
-    // not a multiple of three, the last piece needs to be handed correctly
+    // not odd, the last piece needs to be handed correctly
     std::vector<double> x(4), f(4);
     x[0] = 1;
     x[1] = 3;
@@ -86,8 +88,6 @@ TEST_CASE("Simpson rule on discretized functions.")
 
   SECTION("5 point data set")
   {
-    // Simpson's rule will use three points at a time. If the data sent in is
-    // not a multiple of three, the last piece needs to be handed correctly
     std::vector<double> x(5), f(5);
     x[0] = 1;
     x[1] = 3;
@@ -101,6 +101,124 @@ TEST_CASE("Simpson rule on discretized functions.")
     I = integrate(x, f);
     CHECK(I == Approx(10 * 10 * 10 / 3. + 10 * 10 + 3 * 10 - 1 * 1 * 1 / 3. -
                       1 * 1 - 3 * 1));
+  }
+
+  SECTION("Single-vector dataset")
+  {
+    SECTION("5 point data set")
+    {
+      std::vector<double> f(5);
+      for (std::size_t i = 0; i < f.size(); ++i)
+        f[i] = i*i + 2*i + 3;
+
+      double I;
+      I = integrate(f);
+      CHECK(I == Approx(4.*4.*4./3. + 2.*4.*4./2. + 3.*4.  ));
+    }
+
+    SECTION("6 point data set")
+    {
+      std::vector<double> f(6);
+      for (std::size_t i = 0; i < f.size(); ++i)
+        f[i] = i*i + 2*i + 3;
+
+      double I;
+      I = integrate(f);
+      CHECK(I == Approx(5.*5.*5./3. + 2.*5.*5./2. + 3.*5.  ));
+      I = integrate(f,0.1);
+      CHECK(I == Approx(0.1*(5.*5.*5./3. + 2.*5.*5./2. + 3.*5. )));
+      I = integrate(f,-0.1);
+      CHECK(I == Approx(-0.1*(5.*5.*5./3. + 2.*5.*5./2. + 3.*5. )));
+    }
+  }
+}
+
+TEST_CASE("Simpson rule benchmarks.","[.][benchmarks]")
+{
+  _1D::SimpsonRule<double> integrate;
+  SECTION("Even vs. Odd Datasets")
+  {
+    std::vector<double> f1(5),f2(6),f3(201),f4(202);
+    std::iota(f1.begin(), f1.end(), 0);
+    std::iota(f2.begin(), f2.end(), 0);
+    std::iota(f3.begin(), f3.end(), 0);
+    std::iota(f4.begin(), f4.end(), 0);
+
+    BENCHMARK("5 Point")
+    {
+      return integrate(f1);
+    };
+    BENCHMARK("6 Point")
+    {
+      return integrate(f2);
+    };
+    BENCHMARK("201 Point")
+    {
+      return integrate(f3);
+    };
+    BENCHMARK("202 Point")
+    {
+      return integrate(f4);
+    };
+
+  }
+  SECTION("Uniform vs. Non-Uniform Methods")
+  {
+    std::vector<double> x1(5),x2(6),x3(201),x4(202);
+    std::vector<double> f1(5),f2(6),f3(201),f4(202);
+    std::iota(x1.begin(), x1.end(), 0);
+    std::iota(x2.begin(), x2.end(), 0);
+    std::iota(x3.begin(), x3.end(), 0);
+    std::iota(x4.begin(), x4.end(), 0);
+
+    for(std::size_t i = 0; i < x1.size(); i++)
+      f1[i] = 0.1*x1[i];
+    for(std::size_t i = 0; i < x2.size(); i++)
+      f2[i] = 0.1*x2[i];
+    for(std::size_t i = 0; i < x3.size(); i++)
+      f3[i] = 0.1*x3[i];
+    for(std::size_t i = 0; i < x4.size(); i++)
+      f4[i] = 0.1*x4[i];
+
+    BENCHMARK("5 Point Non-Uniform")
+    {
+      return integrate(x1,f1);
+    };
+    BENCHMARK("5 Point Uniform")
+    {
+      return integrate(f1);
+    };
+
+    BENCHMARK("6 Point Non-Uniform")
+    {
+      return integrate(x2,f2);
+    };
+    BENCHMARK("6 Point Uniform")
+    {
+      return integrate(f2);
+    };
+
+
+    BENCHMARK("201 Point Non-Uniform")
+    {
+      return integrate(x3,f3);
+    };
+    BENCHMARK("201 Point Uniform")
+    {
+      return integrate(f3);
+    };
+
+
+    BENCHMARK("202 Point Non-Uniform")
+    {
+      return integrate(x4,f4);
+    };
+    BENCHMARK("202 Point Uniform")
+    {
+      return integrate(x4,f4);
+    };
+
+
   }
 }
 
