023e0010c9759a35aff636460c4d2e8adba2cf08
CD Clark III <clifton.clark@gmail.com>
2022-02-01 09:08:29 -0600

    housekeeping: updated libInterpolate dependency

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class ConanBuild(ConanFile):
     generators = "cmake", "virtualenv"
-    requires = 'boost/1.72.0', 'libInterpolate/2.3.2@cd3/devel'
+    requires = 'boost/1.72.0', 'libInterpolate/2.3.3@cd3/devel'
 
 
     def build(self):
