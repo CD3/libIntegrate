d0b8f4eb7b15377b1359d5b248e3b2d5513e0839
CD Clark III <clifton.clark@gmail.com>
2025-08-10 18:25:25 -0500

    chore: fixed conan build

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conan import ConanFile
 class Pkg(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain",
     default_options = {'boost/*:header_only ': ' True'}
+    settings = "os", "arch", "compiler", "build_type"
 
     def requirements(self):
         self.requires("boost/1.86.0", force=True)
