77583e9923a8349cc00919108bb0cfc6d942dcae
CD Clark III <clifton.clark@gmail.com>
2025-08-10 18:25:25 -0500

    chore: switched to conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,15 @@
+from conan import ConanFile
+
+class Pkg(ConanFile):
+    generators = "CMakeDeps", "CMakeToolchain",
+    default_options = {'boost/*:header_only ': ' True'}
+
+    def requirements(self):
+        self.requires("boost/1.86.0", force=True)
+        self.requires("libinterpolate/2.6.2")
+        self.requires("catch2/3.3.1")
+
+    def build_requirements(self):
+        pass
+
+
